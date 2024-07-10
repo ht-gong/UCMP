ce492cea0129075d0dcde153eafc12d4935163f5
Haotian Gong <hgong@inet-p4lab-13.mpi-inf.mpg.de>
2024-07-10 19:56:00 +0200

    added descriptions and warnings to scripts

--- run/Figure6bd/runs.sh
+++ run/Figure6bd/runs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Be careful that the following runs are extremely memory-intensive; each run takes up 50 - 100 GBs in RAM.
+
 ../../src/optiroute/datacenter/htsim_ndp_dynexpTopology -utiltime 5e-04 -simtime 0.801 -cutoff 35000000 -rlbflow 0 -cwnd 20 -q 65 -pullrate 1 -alphazero -routing OptiRoute -topfile ../../topologies/slice50us_portion1_queue0_optiroute_adaptive_paths_alpha_0.5.txt -flowfile ../../traffic/datamining_uniform_40percLoad_0.5sec_648hosts.htsim -slicedur 50000000 > OptiRoute_datamining_ndp_0.5sec_0.8sec_50us.txt
 
 ../../src/optiroute/datacenter/htsim_dctcp_dynexpTopology -utiltime 5e-04 -simtime 0.801 -cutoff 35000000 -rlbflow 0 -cwnd 20 -q 300 -alphazero -routing OptiRoute -topfile ../../topologies/slice50us_portion1_queue0_optiroute_adaptive_paths_alpha_0.5.txt -flowfile ../../traffic/datamining_uniform_40percLoad_0.5sec_648hosts.htsim -dctcpmarking 32 -slicedur 50000000 > OptiRoute_datamining_dctcp_0.5sec_0.8sec_50us.txt
